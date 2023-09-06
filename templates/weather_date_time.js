affcf159148d2cfb08259d9a20782913f852b6a5
Viraj Patel <virajpatel325@gmail.com>
2023-09-06 19:15:43 -0400

    code update

--- templates/weather_date_time.js
+++ templates/weather_date_time.js
@@ -1,5 +1,8 @@
 function getCurrentWeather() {
-
+    let url = 'https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&current_weather=true&temperature_unit=fahrenheit&windspeed_unit=mph&precipitation_unit=inch'
+    axios.get(url, {headers: {Accept: "application/json"}}).then(res => {
+        console.log(chalk.dim(symbol + `: `) + chalk.bgGray(res.data["Global Quote"]["05. price"]))
+    })
 }
 
 function getCurrentDateTime() {
